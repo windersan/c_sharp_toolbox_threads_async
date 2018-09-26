42116673265504dff9cc68949a70cf4fa913a867
Devin Andres Salemi <devinsalem@gmail.com>
2018-09-25 19:53:26 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10async
+{
+    public class Class1
+    {
+
+
+        public async Task<int> f1Async()
+        {
+            Task<int> task = new Task<int>(c1);
+            task.Start();
+
+            Console.WriteLine("Intermediate Step");
+
+            int i = await task;
+
+
+            Console.WriteLine(i);
+            return i;
+
+        }
+
+        public int c1()
+        {
+            Thread.Sleep(7000);
+
+            return 1;
+        }
+        public int f1()
+        {
+            int i = c1();
+            Console.WriteLine("Intermediate Step");
+            Console.WriteLine(i);
+            return i;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp10async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k1 = 5;
+            int k2 =5;
+            int k_ref = f_ref(ref k1);
+            int k_k = f_k(k2);
+            WriteLine(k1);
+            WriteLine(k2);
+            
+
+
+            Console.WriteLine("start");
+
+            Class1 c1 = new Class1();
+
+            int z = 0;
+
+            Thread thread = new Thread(()=> {  z = c1.c1(); Console.WriteLine("lol"); });
+
+            thread.Start();
+            thread.Join();
+
+            Console.WriteLine("MAIN");
+            c1.f1Async();
+                
+            try
+            {
+                c1.f1Async().Wait();
+                c1.f1Async().Wait();
+                c1.f1Async().Wait();
+            }
+
+            catch (Exception ex)
+            {
+                
+            }
+
+
+        }
+        public static int f_ref(ref int k)
+        {
+            k = k + 5;
+            return k;
+        }
+        public static int f_k(int k)
+        {
+            k = k + 5;
+            return k;
+        }
+
+
+
+    }
+}
